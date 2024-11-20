0ddc20b14730015e6ebfd8f1ea4c19c7f67f0e8d
SC <lingluosy183@gmail.com>
2024-11-19 20:11:24 -0500

    Initial Project
    a initial scene;
    create scripts:
    playerdata,
    playercontroller,
    player

---
+++ Assets/Scripts/Character/Player.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace rouge2024
+{
+
+    public class Player : MonoBehaviour
+    {
+        public PlayerData playerData;
+        public PlayerController controller;
+
+    }
+}
---
+++ Assets/Scripts/Character/PlayerData.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace rouge2024
+{
+    [Serializable]
+    public class PlayerData
+    {
+        public int hp;
+        public int atk;
+        public int atkRange;
+        public int atkSpeed;
+        public int defense;
+        public int speed;
+        public int hpSteal;
+        public int hpRecover;
+        public int skillHaste;
+    }
+}
