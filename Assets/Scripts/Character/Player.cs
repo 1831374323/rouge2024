81efedccaf45949200432a500213b0b624b0fffc
SC <lingluosy183@gmail.com>
2024-11-20 20:40:13 -0500

    original input system
    create action map;
    function: move;
    create empty funtion: skills,atk

--- Assets/Scripts/Character/Player.cs
+++ Assets/Scripts/Character/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 namespace rouge2024
@@ -9,6 +10,28 @@ namespace rouge2024
     {
         public PlayerData playerData;
         public PlayerController controller;
+
+        private bool isMoving;
+        private Vector3 targetPosition;
+        public void Move(Vector3 _targetPosition)
+        {
+            isMoving = true;
+            targetPosition = _targetPosition;
+        }
+
+        public void Update()
+        {
+            if (isMoving)
+            {
+                float step = playerData.speed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+
+                if (Vector3.Distance(transform.position, targetPosition) < 0.1f)
+                {
+                    isMoving = false;
+                }
+            }
+        }
 
     }
 }
--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -1,21 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace rouge2024
 {
     public class PlayerController : MonoBehaviour
     {
-        
-        void Start()
-        {
+        private Player player;
+        private PlayerData playerData;
 
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            if (context.started)
+            {
+                Vector2 screenPosition = Mouse.current.position.ReadValue();
+                Ray ray = Camera.main.ScreenPointToRay(screenPosition);
+                if (Physics.Raycast(ray, out RaycastHit hit))
+                {
+                    player.Move(hit.point);
+                }
+            }
         }
 
-        
-        void Update()
+        public void OnSkill1(InputAction.CallbackContext context)
         {
-
+            if (context.performed)
+            {
+                Debug.Log("Skill 1 Activated");
+            }
         }
+
+        public void OnSkill2(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                Debug.Log("Skill 2 Activated");
+            }
+        }
+
+        public void OnSkill3(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                Debug.Log("Skill 3 Activated");
+            }
+        }
+
+        public void OnSkill4(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                Debug.Log("Skill 4 Activated");
+            }
+        }
+
+        public void OnAttack(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                Debug.Log("Normal Attack");
+            }
+        }
+
+        #region -----------------lifecycle-----------------
+        void Awake()
+        {
+            player = GetComponent<Player>();
+            playerData = player.playerData;
+        }
+
+
+        #endregion
+
     }
 }
